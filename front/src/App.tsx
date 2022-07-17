c8fb1a1f9be512c6e691ca4dab19e8ae674f455b
abrightkite <phy5397@gmail.com>
2022-07-17 22:00:00 +0900

    Route 수정

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomeContainer from "./components/Home/containers/HomeContainer";
 import SignInContainer from "./components/SignIn/containers/SignInContainer";
 import SignUpContainer from "./components/SignUp/containers/SignUpContainer";
+import SingleGameContainer from "./components/SingleGame/containers/SingleGameContainer";
+import SingleGameListContainer from "./components/SingleGameList/containers/SingleGameListContainer";
 
 function App() {
   return (
@@ -10,8 +12,12 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<SignInContainer />} />
-          <Route path="signup" element={<SignUpContainer />} />
-          <Route path="home" element={<HomeContainer />} />
+          <Route path="/signup" element={<SignUpContainer />} />
+          <Route path="/home" element={<HomeContainer />} />
+          <Route path="/single/*">
+            <Route path="/single/*" element={<SingleGameContainer />} />
+            <Route path="list" element={<SingleGameListContainer />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </div>
---
+++ front/src/components/SingleGameList/SingleGameList.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SingleGameList = () => {
+  return <div className="single-game-list-wrap">single game list page!</div>;
+};
+
+export default SingleGameList;
