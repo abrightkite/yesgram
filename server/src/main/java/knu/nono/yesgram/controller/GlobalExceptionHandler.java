3fe35061228054a74c03395bccc58a6b089eef2d
gidskql6671 <gidskql6671@gmail.com>
2022-07-29 17:26:35 +0900

    적절하지 않은 경로로 요청한 경우를 대응

--- server/src/main/java/knu/nono/yesgram/controller/GlobalExceptionHandler.java
+++ server/src/main/java/knu/nono/yesgram/controller/GlobalExceptionHandler.java
@@ -4,9 +4,13 @@ import knu.nono.yesgram.dto.ErrorResponse;
 import knu.nono.yesgram.exception.ErrorCode;
 import knu.nono.yesgram.exception.ResponseException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @Slf4j
@@ -20,6 +24,16 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 		log.error("handleCommonException : {}", errorCode);
 		ErrorResponse body = ErrorResponse.fromErrorCode(errorCode);
 		
+		return ResponseEntity.status(errorCode.getHttpStatus()).body(body);
+	}
+
+	@Override
+	protected ResponseEntity<Object> handleNoHandlerFoundException(NoHandlerFoundException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+		ErrorCode errorCode = ErrorCode.NOT_FOUND;
+
+		log.error("handleNoHandlerFoundException : {}", errorCode);
+		ErrorResponse body = ErrorResponse.fromErrorCode(errorCode);
+
 		return ResponseEntity.status(errorCode.getHttpStatus()).body(body);
 	}
 }
