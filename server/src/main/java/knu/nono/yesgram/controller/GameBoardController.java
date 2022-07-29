cbe1b410b0e9998566fc5b44ee4376f537e4c3da
gidskql6671 <gidskql6671@gmail.com>
2022-07-29 19:19:37 +0900

    다양한 벨리데이션 에러를 처리

--- server/src/main/java/knu/nono/yesgram/controller/GameBoardController.java
+++ server/src/main/java/knu/nono/yesgram/controller/GameBoardController.java
@@ -6,10 +6,7 @@ import knu.nono.yesgram.exception.NotFoundGameBoardException;
 import knu.nono.yesgram.service.GameBoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
