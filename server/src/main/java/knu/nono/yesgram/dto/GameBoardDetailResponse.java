e59224123a4af221b70810c491159f68e768621a
DongHwan Kim <gidskql6671@gmail.com>
2022-08-06 22:48:50 +0900

    GET /game_board/:board_id 반환 형태 변경 (#22)
    - String으로 제공하던 게임판 데이터를 2차원 배열로 변환

--- server/src/main/java/knu/nono/yesgram/dto/GameBoardDetailResponse.java
+++ server/src/main/java/knu/nono/yesgram/dto/GameBoardDetailResponse.java
@@ -3,6 +3,9 @@ package knu.nono.yesgram.dto;
 import knu.nono.yesgram.domain.GameBoard;
 import lombok.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
@@ -10,13 +13,26 @@ import lombok.*;
 public class GameBoardDetailResponse {
 	private Long id;
 	private int size;
-	private String board;
+	private List<List<Integer>> board;
 	
 	static public GameBoardDetailResponse fromEntity(GameBoard gameBoard) {
+		String answer = gameBoard.getAnswer();
+		int size = gameBoard.getSize();
+		List<List<Integer>> board = new ArrayList<>();
+		
+		for (int i = 0; i < answer.length(); i += size) {
+			List<Integer> row = new ArrayList<>();
+			for(int j = i; j < i + size; j++) {
+				row.add(answer.charAt(j) - '0');
+			}
+			board.add(row);
+		}
+		
+		
 		return new GameBoardDetailResponse(
 				gameBoard.getId(),
-				gameBoard.getSize(),
-				gameBoard.getAnswer()
+				size,
+				board
 		);
 	}
 }
