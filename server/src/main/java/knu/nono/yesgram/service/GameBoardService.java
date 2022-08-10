e1c46f511de2ee671e35ebe5f1b9e3b5575abe4f
gidskql6671 <gidskql6671@gmail.com>
2022-08-10 23:22:30 +0900

    유저 정보를 기반으로 클리어 여부를 판단하도록 수정
    현재 유저 관련된 개발이 되어있지 않아 임시로 모킹작업함

--- server/src/main/java/knu/nono/yesgram/service/GameBoardService.java
+++ server/src/main/java/knu/nono/yesgram/service/GameBoardService.java
@@ -1,7 +1,9 @@
 package knu.nono.yesgram.service;
 
 import knu.nono.yesgram.domain.GameBoard;
+import knu.nono.yesgram.domain.MockUser;
 import knu.nono.yesgram.repository.GameBoardRepository;
+import knu.nono.yesgram.repository.MockUserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -12,22 +14,25 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 public class GameBoardService {
-	
+
 	private final GameBoardRepository gameBoardRepository;
+	private final MockUserRepository userRepository;
 	
 	public Optional<GameBoard> getById(Long id) {
 		return gameBoardRepository.findById(id);
 	}
 	
 	public Page<GameBoard> getGameBoards(Optional<Integer> size, Optional<Boolean> cleared, Pageable pageable) {
+		MockUser user = userRepository.getReferenceById(1L);
+		
 		if (cleared.isEmpty()) {
-			return gameBoardRepository.findGameBoards(size, pageable);
+			return gameBoardRepository.findGameBoards(size, user, pageable);
 		}
 		else if (cleared.get()) {
-			return gameBoardRepository.findClearedGameBoards(size, pageable);
+			return gameBoardRepository.findClearedGameBoards(size, user, pageable);
 		}
 		else {
-			return gameBoardRepository.findUnclearedGameBoards(size, pageable);
+			return gameBoardRepository.findUnclearedGameBoards(size, user, pageable);
 		}
 	}
 }
